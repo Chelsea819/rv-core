0484e433e4d1a751bd0deb731424f509b50b1f0f
tracer-ysyx <tracer@ysyx.org>
2024-11-01 18:11:18 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:11:18 up 22:14,  1 user,  load average: 0.47, 1.09, 1.17

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -141,6 +141,7 @@ static int parse_args(int argc, char *argv[]) {
 }
 
 void init_monitor(int argc, char *argv[]) {
+  printf("init_monitor--pc = %x\n",cpu.pc);
   /* Perform some global initialization. */
 
   /* Parse arguments. */
