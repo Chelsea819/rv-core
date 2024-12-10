23bf3601c77a0f98ae93d28a8cea1acb2fbd7967
tracer-ysyx <tracer@ysyx.org>
2024-12-10 20:16:37 +0800

    > sim RTL ysyx_23060025 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:16:37 up 1 day,  8:52,  1 user,  load average: 0.63, 0.59, 0.48

--- npc/csrc/cpu/difftest/dut.c
+++ npc/csrc/cpu/difftest/dut.c
@@ -27,29 +27,13 @@ void (*ref_difftest_regcpy)(void *duti, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 
-struct is_skip_ref{
-  bool is_skip_ref_bool;
-  vaddr_t is_skip_ref_pc;
-  struct is_skip_ref *next;
-  struct is_skip_ref *past;
-};
-#define NR_SKIP 2
-static struct is_skip_ref *head = NULL;
-static struct is_skip_ref *tail = NULL;
-static struct is_skip_ref skip_pool[2];
-
+static bool is_skip_ref = false;
 static int skip_dut_nr_inst = 0;
 
 // this is used to let ref skip instructions which
 // can not produce consistent behavior with NEMU
 void difftest_skip_ref() {
-  tail->is_skip_ref_bool = true;
-  tail->is_skip_ref_pc = cpu.pc;
-  // printf("tail->is_kskip_ref_pc = 0x%08x cpu.pc = 0x%08x cpu.pc = 0x%08x\n",tail->is_skip_ref_pc,cpu.pc,cpu.pc);
-
-  tail = tail->next;
-  // is_skip_ref = true;
-  // is_skip_ref_pc = cpu.pc;
+  is_skip_ref = true;
   // If such an instruction is one of the instruction packing in QEMU
   // (see below), we end the process of catching up with QEMU's pc to
   // keep the consistent behavior in our best.
@@ -180,13 +164,10 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
   }
 
   //该指令的执行结果以NEMU的状态为准
-  if (head->is_skip_ref_bool && pc == head->is_skip_ref_pc) {
-    // Log("pc = 0x%08x npc = 0x%08x\n",pc,npc);
+  if (is_skip_ref) {
     // to skip the checking of an instruction, just copy the reg state to reference design
     ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
-    head->is_skip_ref_bool = false;
-    head->is_skip_ref_pc = 0;
-    head = head->next;
+    is_skip_ref = false;
     return;
   }
 // ref 0x8000 0x8004 0x8008 0x800c
