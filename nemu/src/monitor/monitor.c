91df7d3cb3e5fcf569684068f1565794865f9cd4
tracer-ysyx <tracer@ysyx.org>
2024-12-11 21:14:42 +0800

    > compile NEMU ysyx_23060025 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:14:42 up 1 day, 20:06,  1 user,  load average: 0.20, 0.34, 0.51

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -24,7 +24,7 @@ typedef struct ftrace_file{
 
 void init_rand();
 void init_log(const char *log_file);
-// void init_mem();
+void init_mem();
 void init_flash();
 void init_psram();
 void init_difftest(char *ref_so_file, long img_size, int port);
@@ -154,9 +154,9 @@ void init_monitor(int argc, char *argv[]) {
   init_log(log_file);
 
   /* Initialize memory. */
-  // init_mem();
+  init_mem();
   
-  init_flash();
+  // init_flash();
 
   init_psram();
 
