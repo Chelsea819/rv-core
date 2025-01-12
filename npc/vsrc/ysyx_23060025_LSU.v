64a39913a022740de4ae897154f99e1553e56546
tracer-ysyx <tracer@ysyx.org>
2025-01-12 15:41:00 +0800

    > sim RTL ysyx_23060025 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:41:00 up 3 days, 20:12,  1 user,  load average: 1.38, 0.67, 0.52

--- npc/vsrc/ysyx_23060025_LSU.v
+++ npc/vsrc/ysyx_23060025_LSU.v
@@ -369,7 +369,7 @@ module ysyx_23060025_LSU #(parameter DATA_LEN = 32,ADDR_LEN = 32)(
 								
     assign mem_rdata = (load_type_i == `LOAD_LB_8)  ? {{24{mem_rdata_unaligned[31]}}, mem_rdata_unaligned[31:24]} : 
                     (load_type_i == `LOAD_LH_16) ? {{16{mem_rdata_unaligned[31]}}, mem_rdata_unaligned[31:16]}: 
-                    (load_type_i == `LOAD_LBU_8) ? {{24{1'b0}}, mem_rdata_unaligned[31:24]}: 
+                    (load_type_i == `LOAD_LBU_8) ? {{24{1'b0}}, mem_rdata_unaligned[7:0]}: 
                     (load_type_i == `LOAD_LHU_16) ? {{16{1'b0}}, mem_rdata_unaligned[31:16]}: 
                     mem_rdata_unaligned;
 
