a815c8d22d35673d67da02a000d0b75a539d9655
tracer-ysyx <tracer@ysyx.org>
2025-01-14 21:34:06 +0800

    > sim RTL ysyx_23060025 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:34:06 up  3:58,  1 user,  load average: 0.93, 0.99, 0.70

--- abstract-machine/scripts/isa/riscv.mk
+++ abstract-machine/scripts/isa/riscv.mk
@@ -1,6 +1,6 @@
 CROSS_COMPILE := riscv64-linux-gnu-
-COMMON_CFLAGS := -fno-pic -march=rv64g -mcmodel=medany -mstrict-align
-CFLAGS        += $(COMMON_CFLAGS) -static
+COMMON_CFLAGS := -fno-pic -march=rv64g -mcmodel=medany -mstrict-align -Os
+CFLAGS        += $(COMMON_CFLAGS) -static -Os 
 ASFLAGS       += $(COMMON_CFLAGS) -Os
 LDFLAGS       += -melf64lriscv
 
