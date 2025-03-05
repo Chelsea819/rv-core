8ba86eaa084992e45c81ff82313b38f721102fc9
Xiaoxue Wang <1938166340@qq.com>
2025-03-05 16:57:16 +0800

    polish registerfile

--- npc/vsrc/ysyx_23060025_RegisterFile.v
+++ npc/vsrc/ysyx_23060025_RegisterFile.v
@@ -19,8 +19,7 @@ module ysyx_23060025_RegisterFile #(parameter ADDR_WIDTH = 16, DATA_WIDTH = 32)(
 	reg [DATA_WIDTH - 1:0] rf [ADDR_WIDTH - 1:0];
 
 	always @(posedge clock) begin
-		if (regWrite && rd != 0) rf[rd[3:0]] <= wdata & (~{32{reset}});
-		else if(regWrite && rd == 0) rf[rd[3:0]] <= 0;
+		if (regWrite && rd != 0) rf[rd[3:0]] <= wdata;
 	end
 
 	//读取操作数
