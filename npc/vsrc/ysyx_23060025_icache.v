031152b9c126a2703df6644a1f23f70dcc0fb556
Xiaoxue Wang <1938166340@qq.com>
2025-03-12 00:42:45 +0800

    128B-2-way random icache

--- npc/vsrc/ysyx_23060025_icache.v
+++ npc/vsrc/ysyx_23060025_icache.v
@@ -6,11 +6,15 @@
  ************************************************************************/
 `include "ysyx_23060025_define.v"
  /* verilator lint_off WIDTHEXPAND */
-module ysyx_23060025_icache #(parameter ADDR_WIDTH = 32, DATA_WIDTH = 32, CACHE_LINE_ADDR_W = 3, CACHE_LINE_OFF_ADDR_W = 4)(
+module ysyx_23060025_icache #(parameter ADDR_WIDTH = 32, DATA_WIDTH = 32, CACHE_LINE_ADDR_W = 2, CACHE_WAY_ADDR_W = 1, CACHE_LINE_OFF_ADDR_W = 4)(
 	input         		clock,
 	input         		reset,
 	// IFU
+`ifdef PC_NO_2
+	input  [29:0] 		in_paddr,	// IFU fetch inst addr
+`else
 	input  [31:0] 		in_paddr,	// IFU fetch inst addr
+`endif 
 	input         		in_psel,	// IFU sel icache
 	output 	        	in_pready,	// icache read data ready
 	output 	 [31:0] 	in_prdata,	// icache read data
@@ -37,7 +41,7 @@ module ysyx_23060025_icache #(parameter ADDR_WIDTH = 32, DATA_WIDTH = 32, CACHE_
 
 	// wire state_idle = (con_state == STATE_IDLE);
 	wire state_check = (con_state == STATE_CHECK);
-	wire state_load = (con_state == STATE_LOAD);
+	// wire state_load = (con_state == STATE_LOAD);
 	// wire state_fence = (con_state == STATE_FENCE);
 `ifdef PC_NO_2
 	wire [29:0]  raddr     = in_paddr;
@@ -45,11 +49,24 @@ module ysyx_23060025_icache #(parameter ADDR_WIDTH = 32, DATA_WIDTH = 32, CACHE_
 	wire [31:0]  raddr     = in_paddr;
 `endif 
 
+	reg random_data;
+	always @(posedge clock) begin
+		if(reset) begin
+			random_data <= 0;
+		end else if(con_state != STATE_LOAD) begin
+			random_data <= random_data ^ 1;
+		end
+	end
+
 	reg	[1:0] con_state;
 	reg	[1:0] next_state;
 
-	reg	[CACHE_LINE_W-1:0]	cache_reg	[CACHE_LINE_NUM-1:0];
-	reg	[TAG_W+CACHE_VALID_W-1:0]			cache_tag	[CACHE_LINE_NUM+CACHE_VALID_W-1:0];
+	reg	[CACHE_LINE_W-1:0]	cache_reg_way_0	[CACHE_LINE_NUM-1:0];
+	reg	[CACHE_LINE_W-1:0]	cache_reg_way_1	[CACHE_LINE_NUM-1:0];
+
+	reg	[TAG_W+CACHE_VALID_W-1:0]			cache_tag_way_0	[CACHE_LINE_NUM+CACHE_VALID_W-1:0];
+	reg	[TAG_W+CACHE_VALID_W-1:0]			cache_tag_way_1	[CACHE_LINE_NUM+CACHE_VALID_W-1:0];
+
 `ifdef PC_NO_2
 	wire [TAG_W-1:0]					addr_tag	= raddr[ADDR_WIDTH-1-2:CACHE_LINE_OFF_ADDR_W+CACHE_LINE_ADDR_W-2];
 	wire [CACHE_LINE_ADDR_W-1:0]		addr_index	= raddr[CACHE_LINE_OFF_ADDR_W+CACHE_LINE_ADDR_W-1-2:CACHE_LINE_OFF_ADDR_W-2];
@@ -60,11 +77,13 @@ module ysyx_23060025_icache #(parameter ADDR_WIDTH = 32, DATA_WIDTH = 32, CACHE_
 	wire [CACHE_LINE_OFF_ADDR_W-1:0]	addr_off	= raddr[CACHE_LINE_OFF_ADDR_W-1:0];
 `endif 
 
-	wire check_hit 					= (addr_tag == cache_tag[addr_index][TAG_W-1:0] && cache_tag[addr_index][TAG_W+CACHE_VALID_W-1] == 1);
+	wire check_hit_0 					= (addr_tag == cache_tag_way_0[addr_index][TAG_W-1:0] && cache_tag_way_0[addr_index][TAG_W+CACHE_VALID_W-1] == 1);
+	wire check_hit_1 					= (addr_tag == cache_tag_way_1[addr_index][TAG_W-1:0] && cache_tag_way_1[addr_index][TAG_W+CACHE_VALID_W-1] == 1);
+	wire check_hit = check_hit_0 | check_hit_1;
 `ifdef PC_NO_2
-	wire [CACHE_LINE_W-1:0] cache_line_data	= cache_reg[addr_index] >> ({addr_off, 2'b0, 3'b0});
+	wire [CACHE_LINE_W-1:0] cache_line_data	= check_hit_0 ? cache_reg_way_0[addr_index] >> ({addr_off, 2'b0, 3'b0}) : cache_reg_way_1[addr_index] >> ({addr_off, 2'b0, 3'b0});
 `else
-	wire [CACHE_LINE_W-1:0] cache_line_data	= cache_reg[addr_index] >> ({addr_off, 3'b0});
+	wire [CACHE_LINE_W-1:0] cache_line_data	= check_hit_0 ? cache_reg_way_0[addr_index] >> ({addr_off, 3'b0}) : cache_reg_way_1[addr_index] >> ({addr_off, 3'b0});
 `endif 
 	wire [DATA_WIDTH-1:0] 	prdata			= cache_line_data[DATA_WIDTH-1:0];
 
@@ -132,17 +151,27 @@ module ysyx_23060025_icache #(parameter ADDR_WIDTH = 32, DATA_WIDTH = 32, CACHE_
 	integer j;
 	always @(posedge clock) begin
 		if(out_rvalid) begin
-			cache_reg[addr_index] <= cache_reg[addr_index] >> 32 | {out_rdata, {(CACHE_LINE_W-32){1'b0}}};
+			if(~random_data) begin
+				cache_reg_way_0[addr_index] <= cache_reg_way_0[addr_index] >> 32 | {out_rdata, {(CACHE_LINE_W-32){1'b0}}};
+			end else begin
+				cache_reg_way_1[addr_index] <= cache_reg_way_1[addr_index] >> 32 | {out_rdata, {(CACHE_LINE_W-32){1'b0}}};
+			end
 		end
 	end
 
 	always @(posedge clock) begin
+		// valid--1
 		if(out_rvalid & out_rlast) begin
-			// valid--1
-			cache_tag[addr_index] <= {1'b1, addr_tag};
+			if(~random_data) begin
+				cache_tag_way_0[addr_index] <= {1'b1, addr_tag};
+			end else begin
+				cache_tag_way_1[addr_index] <= {1'b1, addr_tag};
+			end
+			
 		end else if(next_state == STATE_FENCE) begin
 			for (j = 0; j < CACHE_LINE_NUM; j = j + 1) begin
-				cache_tag[j][TAG_W+CACHE_VALID_W-1] <= 0; // 使用非阻塞赋值
+				cache_tag_way_0[j][TAG_W+CACHE_VALID_W-1] <= 0; // 使用非阻塞赋值
+				cache_tag_way_1[j][TAG_W+CACHE_VALID_W-1] <= 0; // 使用非阻塞赋值
 			end
 		end
 	end
